ec3ffb161d0fa8375fd280c30ef87049017734b4
disha rajput <dishacsevira@gmail.com>
2025-02-10 20:11:43 +0530

    crate music player audio

---
+++ 02_musicplayer/script.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click", function () {
+  const audio = new Audio("audio/sound1.mp3");
+  audio.play();
+});
+document.getElementById("btn2").addEventListener("click", function () {
+  const audio = new Audio("audio/sound2.mp3");
+  audio.play();
+});
+document.getElementById("btn3").addEventListener("click", function () {
+  const audio = new Audio("audio/sound3.mp3");
+  audio.play();
+});
