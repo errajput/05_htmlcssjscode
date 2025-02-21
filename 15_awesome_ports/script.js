852e8cf4c937d9ed020d67b6e7fc989adcf56e1d
disha rajput <dishacsevira@gmail.com>
2025-02-21 17:09:29 +0530

    create awesome port

---
+++ 15_awesome_ports/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const postsContainer = document.querySelector(".posts-container");
+  const APIUrl = "https://jsonplaceholder.typicode.com/posts";
+  async function fetchPosts() {
+    try {
+      const response = await fetch(APIUrl);
+      const posts = await response.json();
+      postsContainer.innerHTML = "";
+      posts.forEach((post) => {
+        const postElement = createPostElement(post);
+        postsContainer.appendChild(postElement);
+      });
+    } catch (error) {
+      postsContainer.innerHTML =
+        " <p style ='color:red; text-align:center;' >Error loading posts. please try again later</p>";
+    }
+  }
+  function createPostElement(post) {
+    const article = document.createElement("article");
+    article.className = "post-card";
+
+    const title = document.createElement("h2");
+    title.className = "post-title";
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.className = "post-body";
+    body.textContent = post.body;
+
+    article.appendChild(title);
+    article.appendChild(body);
+
+    return article;
+  }
+  fetchPosts();
+});
